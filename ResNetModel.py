6b3c6a7032c9e7bf597d92312021e568c6a50ab9
Sebastian Khan <KhanS22@Cardiff.ac.uk>
2020-05-18 18:18:17 +0100

    fix indentation

--- ResNetModel.py
+++ ResNetModel.py
@@ -13,87 +13,88 @@ from tensorflow.keras import layers, models
 
 
 def identity_block(input_tensor, units, bn=False):
-	"""The identity block is the block that has no conv layer at shortcut.
-	# Arguments
-		input_tensor: input tensor
-		units:output shape
-	# Returns
-		Output tensor for the block.
-	"""
-	x = layers.Dense(units)(input_tensor)
+    """The identity block is the block that has no conv layer at shortcut.
+    # Arguments
+            input_tensor: input tensor
+            units:output shape
+    # Returns
+            Output tensor for the block.
+    """
+    x = layers.Dense(units)(input_tensor)
     if bn:
-		x = layers.BatchNormalization()(x)
-	x = layers.Activation('relu')(x)
+        x = layers.BatchNormalization()(x)
+    x = layers.Activation('relu')(x)
 
-	x = layers.Dense(units)(x)
+    x = layers.Dense(units)(x)
     if bn:
-	    x = layers.BatchNormalization()(x)
-	x = layers.Activation('relu')(x)
+        x = layers.BatchNormalization()(x)
+    x = layers.Activation('relu')(x)
 
-	x = layers.Dense(units)(x)
+    x = layers.Dense(units)(x)
     if bn:
-	    x = layers.BatchNormalization()(x)
+        x = layers.BatchNormalization()(x)
 
-	x = layers.add([x, input_tensor])
-	x = layers.Activation('relu')(x)
+    x = layers.add([x, input_tensor])
+    x = layers.Activation('relu')(x)
 
-	return x
+    return x
 
-def dens_block(input_tensor,units, bn=False):
-	"""A block that has a dense layer at shortcut.
-	# Arguments
-		input_tensor: input tensor
-		unit: output tensor shape
-	# Returns
-		Output tensor for the block.
-	"""
-	x = layers.Dense(units)(input_tensor)
+
+def dens_block(input_tensor, units, bn=False):
+    """A block that has a dense layer at shortcut.
+    # Arguments
+            input_tensor: input tensor
+            unit: output tensor shape
+    # Returns
+            Output tensor for the block.
+    """
+    x = layers.Dense(units)(input_tensor)
     if bn:
-	    x = layers.BatchNormalization()(x)
-	x = layers.Activation('relu')(x)
+        x = layers.BatchNormalization()(x)
+    x = layers.Activation('relu')(x)
 
-	x = layers.Dense(units)(x)
+    x = layers.Dense(units)(x)
     if bn:
-	    x = layers.BatchNormalization()(x)
-	x = layers.Activation('relu')(x)
+        x = layers.BatchNormalization()(x)
+    x = layers.Activation('relu')(x)
 
-	x = layers.Dense(units)(x)
+    x = layers.Dense(units)(x)
     if bn:
-	    x = layers.BatchNormalization()(x)
+        x = layers.BatchNormalization()(x)
 
-	shortcut = layers.Dense(units)(input_tensor)
+    shortcut = layers.Dense(units)(input_tensor)
     if bn:
-	    shortcut = layers.BatchNormalization()(shortcut)
+        shortcut = layers.BatchNormalization()(shortcut)
 
-	x = layers.add([x, shortcut])
-	x = layers.Activation('relu')(x)
-	return x
+    x = layers.add([x, shortcut])
+    x = layers.Activation('relu')(x)
+    return x
 
 
 def ResNet50Regression(input_dim=1, output_dim=1, width=16, n_layers=2, bn=False):
-	"""Instantiates the ResNet50 architecture.
-	# Arguments
-        input_dim {1}: X data will have shape, X.shape = (N, intput_dim)
-        output_dim {1}: y data will have shape, y.shape = (N, output_dim)
-		width {16}: width of each layer
-		n_hidden_layers {3}: number of hidden ResNet blocks
-        bn {False}: bool. Use batch normalisation or not after each layer
-	# Returns
-		A Keras model instance.
-	"""
-	Res_input = layers.Input(shape=(input_dim,))
+    """Instantiates the ResNet50 architecture.
+    # Arguments
+    input_dim {1}: X data will have shape, X.shape = (N, intput_dim)
+    output_dim {1}: y data will have shape, y.shape = (N, output_dim)
+            width {16}: width of each layer
+            n_hidden_layers {3}: number of hidden ResNet blocks
+    bn {False}: bool. Use batch normalisation or not after each layer
+    # Returns
+            A Keras model instance.
+    """
+    Res_input = layers.Input(shape=(input_dim,))
 
-	x = dens_block(Res_input,width,bn=bn)
-	x = identity_block(x,width,bn=bn)
-	x = identity_block(x,width,bn=bn)
+    x = dens_block(Res_input, width, bn=bn)
+    x = identity_block(x, width, bn=bn)
+    x = identity_block(x, width, bn=bn)
 
-	for i in range(len(n_layers)):
-		x = dens_block(x,width,bn=bn)
-		x = identity_block(x,width,bn=bn)
-		x = identity_block(x,width,bn=bn)
+    for i in range(len(n_layers)):
+        x = dens_block(x, width, bn=bn)
+        x = identity_block(x, width, bn=bn)
+        x = identity_block(x, width, bn=bn)
 
-	x = layers.BatchNormalization()(x)
-	x = layers.Dense(output_dim,activation='linear')(x)
-	model = models.Model(inputs=Res_input,outputs=x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Dense(output_dim, activation='linear')(x)
+    model = models.Model(inputs=Res_input, outputs=x)
 
-	return model
+    return model
